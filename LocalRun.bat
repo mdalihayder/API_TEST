e2ccab8b52559bf14ece7e961b1d3c1ebafc0cb9
mdalihayder <ali_usa@outlook.com>
2019-04-13 11:26:05 -0400

    testing

--- LocalRun.bat
+++ LocalRun.bat
@@ -1,2 +1,2 @@
 cd SoapUI-5.0.0\bin
-testrunner.bat -sTestSuite -r -j -fReports C:\Users\M\.jenkins\workspace\SoapUI-Test\SoapUI-5.0.0\Sample-SOAP-Project-soapui-project.xml
+testrunner.bat -sTestSuite -r -j -fReports C:\Users\M\.jenkins\workspace\SoapUI-Test\Sample-SOAP-Project-soapui-project.xml
