890c005a7a8ace79f2303ad2bb0494f57ad253bc
mdalihayder <ali_usa@outlook.com>
2019-04-13 12:04:59 -0400

    UPDATED

--- Run.bat
+++ Run.bat
@@ -1,2 +1,2 @@
 cd SoapUI-5.0.0\bin
-testrunner.bat -sTestSuite -r -j -fReports ${WORKSPACE}\Sample-SOAP-Project-soapui-project.xml
+testrunner.bat -sTestSuite -r -j -fReports C:\Users\M\.jenkins\workspace\SoapUI-Test\SoapUI-5.0.0\bin\ProjectXML\Sample-SOAP-Project-soapui-project.xml
